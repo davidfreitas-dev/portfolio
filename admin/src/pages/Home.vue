61502186e15c0bcd01dda5ec9499998b3580d144
David Freitas <davidfreitas.dev@outlook.com>
2024-08-05 16:39:55 -0300

    fix deploy file

--- admin/src/pages/Home.vue
+++ admin/src/pages/Home.vue
@@ -8,7 +8,7 @@ import MainContainer from '../components/shared/MainContainer.vue';
       Home
     </h1>
     <p class="text-secondary">
-      Bem-vindo à página inicial
+      Bem-vindo 
     </p>
   </MainContainer>
 </template>
